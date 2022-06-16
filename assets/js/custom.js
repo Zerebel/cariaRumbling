71072060051a91da032961d5d168a789bf125731
Truth-hash <86872654+Truth-hash@users.noreply.github.com>
2022-06-16 01:46:40 +0000

    crete testimony

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,3 +29,45 @@ document.addEventListener("scroll", debounce(storeScroll), { passive: true });
 // Update scroll position for first time
 storeScroll();
 
+(function ($) {
+  "use strict";
+
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight();
+
+  var carousel = function () {
+    $(".featured-carousel").owlCarousel({
+      loop: true,
+      autoplay: true,
+      autoHeight: false,
+      margin: 30,
+      animateOut: "slideOutDown",
+      animateIn: "flipInX",
+      nav: true,
+      dots: true,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<p><small>Prev</small><span class='ion-ios-arrow-round-back'></span></p>",
+        "<p><small>Next</small><span class='ion-ios-arrow-round-forward'></span></p>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 1,
+        },
+      },
+    });
+  };
+  carousel();
+})(jQuery);
