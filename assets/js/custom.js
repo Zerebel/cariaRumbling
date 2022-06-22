a1b3d3d07573294451c0ce810b41384965c06554
Truth-hash <86872654+Truth-hash@users.noreply.github.com>
2022-06-22 12:25:28 +0000

    404-page-done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -35,3 +35,10 @@ var downloadTimer = setInterval(function () {
   document.getElementById("countdowntimer").textContent = timeleft;
   if (timeleft <= 0) clearInterval(downloadTimer);
 }, 1000);
+
+var timelefts = 6;
+var downloadTimers = setInterval(function () {
+  timelefts--;
+  document.getElementById("countdowntimer2").textContent = timelefts;
+  if (timelefts <= 0) clearInterval(downloadTimers);
+}, 1000);
